4c4bf404dc7107f25a8e571fe33f7e117674ec07
dhakalmanish20 <dhakalmanish20@gmail.com>
2025-02-14 22:01:33 +0545

    Fix error in Register and improved the UI

--- monitoring_app/form_tags.py
+++ monitoring_app/form_tags.py
@@ -1,7 +1,18 @@
-from django import template
-
-register = template.Library()
-
-@register.filter(name='add_class')
-def add_class(field, css_class):
-    return field.as_widget(attrs={'class': css_class})
+from django import template  
+  
+register = template.Library()  
+  
+@register.filter(name='add_attributes')  
+def add_attributes(field, args):  
+    # Split the arguments by commas  
+    attrs = {}  
+    for arg in args.split(','):  
+        # Split key and value  
+        key_value = arg.strip().split('=')  
+        if len(key_value) == 2:  
+            key, value = key_value  
+            attrs[key.strip()] = value.strip()  
+        elif len(key_value) == 1:  
+            # For arguments like 'required'  
+            attrs[key_value[0].strip()] = True  
+    return field.as_widget(attrs=attrs)  
